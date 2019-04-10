622160f285d4da6550148499fccb3ab0e794168b
Bogdan Kolbov <kolbov@niiet.ru>
2019-04-10 08:56:29 +0300

    Добавил слоты и ресурсы

--- app/k1921vkx_flasher.py
+++ app/k1921vkx_flasher.py
@@ -29,7 +29,78 @@ class MainWindow(QMainWindow):
     def closeEvent(self, event):
         event.accept()
 
-    # -- Slots --
+    # -- Slots general --
+    def handle_act_about_triggered(self):
+        pass
+
+    def handle_combo_port_activated(self):
+        pass
+
+    def handle_combo_port_changed(self, num):
+        pass
+
+    def handle_combo_baud_changed(self, num):
+        pass
+
+    def handle_btn_connect_clicked(self):
+        pass
+
+    def handle_flash_select_toggled(self, state):
+        pass
+
+    def handle_btn_exec_clicked(self):
+        pass
+
+    # -- Slots for Write Tab widgets --
+    def handle_twrite_ledit_filepath_editint_finished(self):
+        pass
+
+    def handle_twrite_ledit_addr_editing_finished(self):
+        pass
+
+    def handle_twrite_btn_fileopen_clicked(self):
+        pass
+
+    # -- Slots for Erase Tab widgets --
+    def handle_terase_ledit_firstpage_editing_finished(self):
+        pass
+
+    def handle_terase_ledit_lastpage_editing_finished(self):
+        pass
+
+    def handle_terase_ledit_addrstart_editing_finished(self):
+        pass
+
+    def handle_terase_ledit_bytes_editing_finished(self):
+        pass
+
+    # -- Slots for Read Tab widgets --
+    def handle_tread_ledit_filepath_editing_finished(self):
+        pass
+
+    def handle_tread_btn_fileopen_clicked(self):
+        pass
+
+    def handle_tread_ledit_firstpage_editing_finished(self):
+        pass
+
+    def handle_tread_ledit_lastpage_editing_finished(self):
+        pass
+
+    def handle_tread_ledit_addrstart_editing_finished(self):
+        pass
+
+    def handle_tread_ledit_bytes_editing_finished(self):
+        pass
+
+    def handle_tread_chbox_verif_toggled(self, state):
+        pass
+
+    def handle_tread_ledit_verif_filepath_editing_finished(self):
+        pass
+
+    def handle_tread_btn_verif_fileopen_clicked(self):
+        pass
 
     # -- Application specific code --
 
