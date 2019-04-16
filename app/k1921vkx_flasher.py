22410792d7850aab1247f1fbe2809086b28cd22d
Bogdan Kolbov <kolbov@niiet.ru>
2019-04-16 11:54:36 +0300

    Обновил страницу конфигурации для ВК01Т

--- app/k1921vkx_flasher.py
+++ app/k1921vkx_flasher.py
@@ -37,8 +37,8 @@ def whoami():
 def test_init(port, baud):
     log_info("Открываю порт %s %s" % (port, baud))
     # return {'chipid': '0x5A298FE1', 'cpuid': '0xDEADBEEF', 'bootver': '0.1'}
-    return {'chipid': '0x3ABF2FD1', 'cpuid': '0xDEADBEEF', 'bootver': '0.1'}
-    #return {'chipid': '0x00000000', 'cpuid': '0xDEADBEEF', 'bootver': '0.1'}
+    # return {'chipid': '0x3ABF2FD1', 'cpuid': '0xDEADBEEF', 'bootver': '0.1'}
+    return {'chipid': '0x00000000', 'cpuid': '0xDEADBEEF', 'bootver': '0.1'}
 
 
 def test_deinit():
