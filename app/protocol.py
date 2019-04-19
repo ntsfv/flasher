a61ec858d05c698c1eb23e524c0c1b1f15b5537b
Bogdan Kolbov <kolbov@niiet.ru>
2019-04-19 15:31:02 +0300

    Минорные фиксы интерфейса

--- app/protocol.py
+++ app/protocol.py
@@ -30,9 +30,9 @@ def init(win, **kwargs):
     log_dbg(win, "%s->%s()" % (os.path.basename(__file__), win.whoami()))
     log_dbg(win, kwargs)
     log_info(win, "Открываю порт %s %s" % (kwargs['port'], kwargs['baud']))
-    # return {'chipid': '0x5A298FE1', 'cpuid': '0xDEADBEEF', 'bootver': '0.1'}
+    return {'chipid': '0x5A298FE1', 'cpuid': '0xDEADBEEF', 'bootver': '0.1'}
     # return {'chipid': '0x3ABF2FD1', 'cpuid': '0xDEADBEEF', 'bootver': '0.1'}
-    return {'chipid': '0x00000000', 'cpuid': '0xDEADBEEF', 'bootver': '0.1'}
+    # return {'chipid': '0x00000000', 'cpuid': '0xDEADBEEF', 'bootver': '0.1'}
 
 
 def deinit(win, **kwargs):
